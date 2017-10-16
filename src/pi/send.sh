ba7232bffc2cfac21cd1ba7b25b669be805d4e8d
Anirudh Bisht <anirudh.bisht@gmail.com>
2017-10-16 15:37:59 +0200

    Initial Codebase, Implemented easy sending of files to PI from dev machine

---
+++ src/pi/send.sh
@@ -0,0 +1,8 @@
+#@author Anirudh Bisht
+#@brief This script send the files from dev machine to Pi
+## Add files you want on the pi to the dependency mentioned below
+## Keep dependencies clean , line sperated
+##If you want to avoid typing your password everytime add your public key to pi authorized keys(~/.ssh/authorized_keys)
+DEPENDENCYFILE=deps.txt
+
+cat $DEPENDENCYFILE | xargs -Iz scp z pi@raspberrypi.local:~/SolarPatterns/
